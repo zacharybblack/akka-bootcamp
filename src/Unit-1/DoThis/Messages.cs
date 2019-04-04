3dc470f6510873bb68cdbae940e42f405891df18
Zachary Black <zblack@omnisite.com>
2019-04-04 11:46:00 -0400

    Completed Unit 1 Lesson 2

---
+++ src/Unit-1/DoThis/Messages.cs
@@ -0,0 +1,53 @@
+﻿namespace WinTail
+{
+    class Messages
+    {
+        #region Neutral/system messages
+
+        public class ContinueProcessing
+        {
+        }
+
+        #endregion
+
+        #region Success messages
+
+        public class InputSuccess
+        {
+            public string Reason { get; private set; }
+
+            public InputSuccess(string reason)
+            {
+                Reason = reason;
+            }
+        }
+        #endregion
+
+        #region Error messages
+
+        public class InputError
+        {
+            public string Reason { get; private set; }
+
+            public InputError(string reason)
+            {
+                Reason = reason;
+            }
+        }
+
+        public class NullInputError : InputError
+        {
+            public NullInputError(string reason) : base(reason)
+            {
+            }
+        }
+
+        public class ValidationError : InputError
+        {
+            public ValidationError(string reason) : base(reason)
+            {
+            }
+        }
+        #endregion
+    }
+}
